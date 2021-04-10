dbc2117a273b845b05a372baa572061d7eacab20
alexandermak <alexandermrak23@gmail.com>
2021-04-10 17:05:21 -0500

    creats edit function, and view. Starts update and delete function

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -9,6 +9,8 @@ module.exports = {
   new: newTask,
   create,
   delete: deleteOne,
+  edit,
+  update,
 };
 
 function index(req, res) {
@@ -35,4 +37,25 @@ function create(req, res) {
   res.redirect("/tasks");
 }
 
-function deleteOne(req, res) {}
+function deleteOne(req, res) {
+  Task.findByIdAndRemove(req.params.id, function(err) {
+    if (err) return res.redirect("/tasks/show");
+  });
+  res.redirect("/tasks");
+}
+
+function edit(req, res) {
+  Task.findById(req.params.id, function (err, task) {
+    if (err) return res.redirect("/tasks/show");
+    res.render("tasks/edit", { title: "Edit Task", task});
+  });
+}
+
+function update(req, res) {
+  console.log("works");
+  Task.findByIdAndUpdate(req.params.id, req.body)
+    // function (err) {
+    // if (err) return res.redirect("/");
+  // });
+  res.redirect("/tasks");
+}
--- routes/tasks.js
+++ routes/tasks.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const tasksCtrl = require("../controllers/tasks");
 
 // remove 'tasks' from route when we specify in the server
+router.get("/new", tasksCtrl.new);
 router.get("/", tasksCtrl.index);
 router.get("/:id", tasksCtrl.show);
-router.get("/new", tasksCtrl.new);
 router.post("/", tasksCtrl.create);
+router.get("/:id/edit", tasksCtrl.edit);
+router.put("/:id", tasksCtrl.update);
 // router.delete("/:id", tasksCtrl.delete);
 
 module.exports = router;
